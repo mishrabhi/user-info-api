4de142199c6d6b856372eb17e863b2c0bf54b1f4
mishrabhi <asmyselfabhishek00@gmail.com>
2025-02-14 00:08:42 +0530

    established db connection

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+dotenv.config(); //environment variables from .env file
+
+connectDB(); //Database connection
 
 const app = express();
-app.use(express.json());
 
+//middleware setup
+app.use(express.json());
+app.use(cors());
+
+//Start the server
 app.listen(8080, () => {
   console.log("Server up and running");
 });
